e1525bf18037a06534378330793b7b3fd0f5d3db
Kier Lomugdang <Kier.Lomugdang@monash.edu>
2019-11-30 14:41:40 +1100

    added firebase sdk and now pulls data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from '../src/firebaseConfig.js'
 
 function App() {
+
+  const db = firebase.firestore();
+  const userRef = db.collection("users").doc("sayuj")
+
+  userRef.get().then(doc => {
+    if (doc.exists) {
+      console.log(doc.data())
+    } else {
+      console.log("document does not exist")
+    }
+  })
+    .catch(error => {
+      console.log(error)
+    })
+
   return (
     <div className="App">
       <header className="App-header">
