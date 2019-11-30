5fc54d593295ec4b460edeb206462b2c115d82f3
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-11-30 14:44:47 +1100

    Added initial

---
+++ src/pages/landing/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styled from './index.module.scss';
+
+const LandingPage = React.memo(() => {
+    
+    return (
+        <div className={styled.gridContainer}>
+            
+        </div>
+    )
+});
+
+
+export default LandingPage;
