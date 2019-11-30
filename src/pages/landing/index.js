b647c63901861075a22ba872a2e3b1e6610e4fff
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-11-30 15:31:23 +1100

    ASD

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,12 +1,32 @@
 import React from "react";
-
+import clsx from "clsx";
+import HamBurgerMenu from '../../assets/icons/menu.svg';
 import styled from './index.module.scss';
 
 const LandingPage = React.memo(() => {
     
     return (
         <div className={styled.gridContainer}>
-            
+            <div className={styled.top}>
+                <div className={styled.navbar}>
+                    <div className={styled.circleContainer} >
+                        <img className={styled.hamburgerMenu} src={HamBurgerMenu} />
+                    </div>
+                    <div className={styled.circleContainer} >
+                        <img className={styled.hamburgerMenu} src={HamBurgerMenu} />
+                    </div>
+                </div>
+            </div>
+            <div className={styled.bottom}>
+                <div className={styled.tableList}>
+                  <div>1</div>
+                  <div>2</div>
+                  <div>3</div>
+                  <div>4</div>
+                  <div>5</div>
+                  <div>6</div>
+                </div>
+            </div>
         </div>
     )
 });
