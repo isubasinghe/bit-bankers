7b892f06dea65d954d0a9d4c1b678a03b4c7c0aa
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-11-30 15:40:11 +1100

    Added things

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -18,14 +18,7 @@ const LandingPage = React.memo(() => {
                 </div>
             </div>
             <div className={styled.bottom}>
-                <div className={styled.tableList}>
-                  <div>1</div>
-                  <div>2</div>
-                  <div>3</div>
-                  <div>4</div>
-                  <div>5</div>
-                  <div>6</div>
-                </div>
+               
             </div>
         </div>
     )
