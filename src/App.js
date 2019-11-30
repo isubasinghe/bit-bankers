5dcf43125fbc1deae1786f41a38d01c16030a34e
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-11-30 18:04:01 +1100

    Added files

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import firebase from '../src/firebaseConfig.js'
-import LandingPage from './pages/Home';
-import './App.css';
-
+import firebase from "../src/firebaseConfig.js";
+import LandingPage from "./pages/Home";
+import Debug from "./components/donations";
+import "./App.scss";
 
 function App() {
-
   const db = firebase.firestore();
-  const userRef = db.collection("users").doc("sayuj")
+  const userRef = db.collection("users").doc("sayuj");
 
-  userRef.get().then(doc => {
-    if (doc.exists) {
-      console.log(doc.data())
-    } else {
-      console.log("document does not exist")
-    }
-  })
-    .catch(error => {
-      console.log(error)
+  userRef
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        console.log(doc.data());
+      } else {
+        console.log("document does not exist");
+      }
     })
+    .catch(error => {
+      console.log(error);
+    });
 
   return (
-   <Router>
-     <Route exact path="/" component={LandingPage}/>
-   </Router>
+    <Router>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/debug" component={Debug} />
+    </Router>
   );
 }
 
